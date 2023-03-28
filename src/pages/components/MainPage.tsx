ddd8f83a69ccc2389a9c9fc8ad410191999d2dc4
Jacobbidmead <jacob.bidmead.fullstack@outlook.com>
2023-03-28 12:49:00 +0100

    Added latest layout and initial images

--- src/pages/components/MainPage.tsx
+++ src/pages/components/MainPage.tsx
@@ -10,7 +10,70 @@ const MainPage = () => {
           <span className="welcome-message">Welcome.</span>
         </div>
         <HomeSwiper images={mainImg} />
-        <div className="latest-container"></div>
+        <div className="latest-container">
+          <div className="latest-img-container">
+            <img
+              src="https://cdn.shopify.com/s/files/1/1202/6102/products/nike-sb-janoski-og_-shoes-alabaster-alabaster-chile-red-1_600x696_crop_center.progressive.jpg?v=1679483172"
+              alt=""
+              className="latest-img"
+            />
+          </div>
+          <div className="latest-img-container">
+            {" "}
+            <img
+              src="https://cdn.shopify.com/s/files/1/1202/6102/products/polar-big-boy-jeans-light-blue-3_37c29c94-1093-4f50-bba3-1bded6772014_600x696_crop_center.progressive.jpg?v=1647261599"
+              alt=""
+              className="latest-img"
+            />
+          </div>
+          <div className="latest-img-container">
+            {" "}
+            <img
+              src="https://cdn.shopify.com/s/files/1/1202/6102/products/polar-big-boy-jeans-light-blue-3_37c29c94-1093-4f50-bba3-1bded6772014_600x696_crop_center.progressive.jpg?v=1647261599"
+              alt=""
+              className="latest-img"
+            />
+          </div>
+          <div className="latest-img-container">
+            {" "}
+            <img
+              src="https://cdn.shopify.com/s/files/1/1202/6102/products/polar-big-boy-jeans-light-blue-3_37c29c94-1093-4f50-bba3-1bded6772014_600x696_crop_center.progressive.jpg?v=1647261599"
+              alt=""
+              className="latest-img"
+            />
+          </div>
+          <div className="latest-img-container">
+            <img
+              src="https://cdn.shopify.com/s/files/1/1202/6102/products/nike-sb-janoski-og_-shoes-alabaster-alabaster-chile-red-1_600x696_crop_center.progressive.jpg?v=1679483172"
+              alt=""
+              className="latest-img"
+            />
+          </div>
+          <div className="latest-img-container">
+            {" "}
+            <img
+              src="https://cdn.shopify.com/s/files/1/1202/6102/products/polar-big-boy-jeans-light-blue-3_37c29c94-1093-4f50-bba3-1bded6772014_600x696_crop_center.progressive.jpg?v=1647261599"
+              alt=""
+              className="latest-img"
+            />
+          </div>
+          <div className="latest-img-container">
+            {" "}
+            <img
+              src="https://cdn.shopify.com/s/files/1/1202/6102/products/polar-big-boy-jeans-light-blue-3_37c29c94-1093-4f50-bba3-1bded6772014_600x696_crop_center.progressive.jpg?v=1647261599"
+              alt=""
+              className="latest-img"
+            />
+          </div>
+          <div className="latest-img-container">
+            {" "}
+            <img
+              src="https://cdn.shopify.com/s/files/1/1202/6102/products/polar-big-boy-jeans-light-blue-3_37c29c94-1093-4f50-bba3-1bded6772014_600x696_crop_center.progressive.jpg?v=1647261599"
+              alt=""
+              className="latest-img"
+            />
+          </div>
+        </div>
       </div>
     </>
   );
