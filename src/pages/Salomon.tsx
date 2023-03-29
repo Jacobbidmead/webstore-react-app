a6dc7728767c9b5f0033435b5e8f3589f92661f1
Jacobbidmead <jacob.bidmead.fullstack@outlook.com>
2023-03-29 13:15:17 +0100

    Adjusted Salomon JSON file
    to array instead if object of arrays, this way I can map over the products

--- src/pages/Salomon.tsx
+++ src/pages/Salomon.tsx
@@ -1,9 +1,11 @@
 import { useState, FC } from "react";
+import salomonData from "../data/salomonProd.json";
 
 const Salomon: FC = () => {
+  console.log(salomonData);
   return (
     <>
-      <div>hello world</div>
+      <div></div>
     </>
   );
 };
