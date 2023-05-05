8db85fca9de3c3432af678d0a97b033f77a23c8f
Jacob Bidmead <107250881+Jacobbidmead@users.noreply.github.com>
2023-05-05 12:34:54 +0100

    Added basket login to Salomon page
    - Select with sizes, add to basket button adds product name, colour and size to basket

--- src/pages/Salomon.tsx
+++ src/pages/Salomon.tsx
@@ -1,8 +1,28 @@
 import { useState, FC } from "react";
 import salomonData from "../data/salomonProd.json";
 
+interface Product {
+  id: number;
+  name: string;
+  color: string;
+  size: string[];
+  price: number;
+  imgUrl: string;
+}
+
 const Salomon: FC = () => {
-  
+  const [selectedSize, setSelectedSize] = useState("");
+  const [basket, setBasket] = useState([]);
+  const [basketTotal, setBasketTotal] = useState(0);
+
+  const addToBasket = (product: Product) => {
+    if (selectedSize !== "") {
+      setBasket([...basket, { product, size: selectedSize }]);
+      setBasketTotal(basketTotal + product.price);
+      setSelectedSize("");
+    }
+  };
+
   return (
     <>
       <div className="store-main-container">
@@ -13,22 +33,44 @@ const Salomon: FC = () => {
         <div>
           <div className="store-items-container">
             {salomonData.map((item) => (
-              <div>
-                <img src={item.imgUrl} alt="" className="product-image"/>
+              <div key={item.id}>
+                <img src={item.imgUrl} alt="" className="product-image" />
                 <div>{item.name}</div>
                 <div>{item.color}</div>
                 <div>{item.price}</div>
-                <select>
-                  <option value="7" >7</option>
-                  <option value="8"  >8</option>
-                  <option value="9" >9</option>
-                  <option value="10">10</option>
-                  <option value="11">11</option>
-                </select>
-                <button>add to basket</button>
+                <form
+                  onSubmit={(event) => {
+                    event.preventDefault();
+                  }}
+                >
+                  <select
+                    value={selectedSize}
+                    onChange={(event) => setSelectedSize(event.target.value)}
+                  >
+                    <option value="">Select a size</option>
+                    {item.size.map((size, index) => (
+                      <option key={index} value={size}>
+                        {size}
+                      </option>
+                    ))}
+                  </select>
+                  <button type="button" onClick={() => addToBasket(item)}>
+                    Add to basket
+                  </button>
+                </form>
               </div>
             ))}
           </div>
+        </div>
+        <div>
+          <h2>Basket</h2>
+          <ul>
+            {basket.map(({ product, size }, index) => (
+              <li key={index}>
+                {product.name} - {product.color} - {size} - ${product.price}
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
     </>
