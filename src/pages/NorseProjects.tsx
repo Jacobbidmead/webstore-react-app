08080a80b60c18fe959f8b3947cd9ebee0975929
Jacob Bidmead <107250881+Jacobbidmead@users.noreply.github.com>
2023-05-11 13:45:16 +0100

    Added top bar to Norse Projects page

--- src/pages/NorseProjects.tsx
+++ src/pages/NorseProjects.tsx
@@ -2,6 +2,12 @@ import { useState, FC, useContext } from "react";
 import norseProjectsData from "../data/norseprojectsProd.json";
 import Basket from "./components/Basket";
 import { ShopContext } from "@/context/shop-context";
+import IconButton from "@mui/material/IconButton";
+import Link from "next/link";
+import Toolbar from "@mui/material/Toolbar";
+import { AppBar } from "@mui/material";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import HomeIcon from "@mui/icons-material/Home";
 
 interface Product {
   id: number;
@@ -26,6 +32,26 @@ const NorseProjects: FC = () => {
 
   return (
     <>
+      <AppBar position="fixed">
+        <Toolbar
+          sx={{
+            // backgroundColor: "#fbfbfbcc",
+            justifyContent: "space-between",
+          }}
+        >
+          <Link href="/">
+            <IconButton component="a">
+              <HomeIcon />
+            </IconButton>
+          </Link>
+
+          <Link href="/BasketPage">
+            <IconButton component="a">
+              <ShoppingCartIcon sx={{ cursor: "pointer" }} />
+            </IconButton>
+          </Link>
+        </Toolbar>
+      </AppBar>
       <div>
         <Basket basket={basket} setBasket={setBasket} />
       </div>
@@ -33,7 +59,15 @@ const NorseProjects: FC = () => {
         <div className="store-brand-info">
           <h1>Norse Projects</h1>
         </div>
-        <div>Norse Projects description</div>
+        <div>
+          Discover Norse Projects, the epitome of timeless Scandinavian design
+          and contemporary style. With a carefully curated collection of premium
+          clothing and accessories, their web store blends Nordic craftsmanship
+          with a modern aesthetic. From clean lines to exceptional attention to
+          detail, each piece embodies functionality and minimalist elegance.
+          Experience the perfect fusion of versatility and sophistication at
+          Norse Projects, where understated luxury meets enduring quality.
+        </div>
         <div>
           <div className="store-items-container">
             {norseProjectsData.map((item) => (
