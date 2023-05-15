4619fd98cec6e98be3f1611a64a69375479d706f
Jacob Bidmead <107250881+Jacobbidmead@users.noreply.github.com>
2023-05-15 13:37:26 +0100

    Created Polat JSON file
    - Added data to Polar page

--- src/pages/Polar.tsx
+++ src/pages/Polar.tsx
@@ -1,5 +1,5 @@
 import { useState, FC, useContext } from "react";
-import norseProjectsData from "../data/norseprojectsProd.json";
+import polarData from "../data/polarProd.json";
 import Basket from "./components/Basket";
 import { ShopContext } from "@/context/shop-context";
 import IconButton from "@mui/material/IconButton";
@@ -57,25 +57,15 @@ const Polar: FC = () => {
           </Link>
         </Toolbar>
       </AppBar>
-      <div>
-        <Basket basket={basket} setBasket={setBasket} />
-      </div>
+
       <div className="store-main-container">
         <div className="store-brand-info">
-          <h1>Norse Projects</h1>
+          <h1>Polar</h1>
         </div>
-        <div>
-          Discover Norse Projects, the epitome of timeless Scandinavian design
-          and contemporary style. With a carefully curated collection of premium
-          clothing and accessories, their web store blends Nordic craftsmanship
-          with a modern aesthetic. From clean lines to exceptional attention to
-          detail, each piece embodies functionality and minimalist elegance.
-          Experience the perfect fusion of versatility and sophistication at
-          Norse Projects, where understated luxury meets enduring quality.
-        </div>
+        <div>Polar descritption</div>
         <div>
           <div className="store-items-container">
-            {norseProjectsData.map((item) => (
+            {polarData.map((item) => (
               <div key={item.id}>
                 <img src={item.imgUrl} alt="" className="product-image" />
                 <div>{item.name}</div>
