62ce9a7a78b5c0b76d92c1d681a3a86e2a5d76c5
Jacob Bidmead <107250881+Jacobbidmead@users.noreply.github.com>
2023-05-22 12:21:38 +0100

    Added top bar to basket page
    - Adjusted responsiveness of basket page.
    - Made small adjustments to the styling

--- src/pages/components/Basket.tsx
+++ src/pages/components/Basket.tsx
@@ -1,6 +1,12 @@
 import { FC, useContext } from "react";
 import { ShopContext } from "@/context/shop-context";
 import ClearIcon from "@mui/icons-material/Clear";
+import IconButton from "@mui/material/IconButton";
+import Link from "next/link";
+import Toolbar from "@mui/material/Toolbar";
+import { AppBar } from "@mui/material";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import HomeIcon from "@mui/icons-material/Home";
 
 interface Product {
   id: number;
@@ -26,28 +32,47 @@ const Basket: FC<Props> = ({ basket }) => {
   const { removeFromBasket, totalPrice } = useContext(ShopContext)!;
 
   return (
-    <div className="basket-container">
-      <div className="basket-header">
-        <h3>Basket</h3>
-      </div>
-      <div>
-        {basket.map((item, index) => (
-          <div className="basket-table" key={index}>
-            <img className="basket-img" src={item.product.imgUrl} alt="" />
-            <div>{item.product.name}</div>
-            <div> {item.product.color}</div> {item.size}{" "}
-            <div>£{item.product.price}</div>
-            <div>
-              {" "}
-              <ClearIcon onClick={() => removeFromBasket(item)}>
-                Delete
-              </ClearIcon>
-            </div>
+    <>
+      <AppBar position="fixed">
+        <Toolbar
+          sx={{
+            // backgroundColor: "#fbfbfbcc",
+            justifyContent: "space-between",
+          }}
+        >
+          <Link href="/">
+            <IconButton>
+              <HomeIcon />
+            </IconButton>
+          </Link>
+          <div className="basket-counter">
+            ({basket.length > 0 && <span>{basket.length}</span>})
           </div>
-        ))}
-        <div className="basket-total">Total: £{totalPrice}</div>
+        </Toolbar>
+      </AppBar>
+      <div className="basket-container">
+        <div className="basket-header">
+          <h3>Basket</h3>
+        </div>
+        <div>
+          {basket.map((item, index) => (
+            <div className="basket-table" key={index}>
+              <img className="basket-img" src={item.product.imgUrl} alt="" />
+              <div>{item.product.name}</div>
+              <div> {item.product.color}</div> {item.size}{" "}
+              <div>£{item.product.price}</div>
+              <div>
+                {" "}
+                <ClearIcon onClick={() => removeFromBasket(item)}>
+                  Delete
+                </ClearIcon>
+              </div>
+            </div>
+          ))}
+          <div className="basket-total">Total: £{totalPrice}</div>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
