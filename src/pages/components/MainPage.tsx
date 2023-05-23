010a7c080d16e203ebcd2b62e075a8ce7c517fb4
Jacob Bidmead <107250881+Jacobbidmead@users.noreply.github.com>
2023-05-23 12:46:18 +0100

    Featured images are now links

--- src/pages/components/MainPage.tsx
+++ src/pages/components/MainPage.tsx
@@ -10,7 +10,7 @@ const MainPage: FC = () => {
         <HomeSwiper images={mainImg} />
         <span className="latest-header">Featured</span>
         <div className="latest-container">
-          <div className="latest-img-container">
+          <Link href="/Nike" className="latest-img-container">
             <img
               src="https://cdn.shopify.com/s/files/1/1202/6102/products/nike-sb-janoski-og_-shoes-alabaster-alabaster-chile-red-1_600x696_crop_center.progressive.jpg?v=1679483172"
               alt=""
@@ -19,8 +19,8 @@ const MainPage: FC = () => {
             <div className="middle-text">
               <div>Nike Janoski OG</div>
             </div>
-          </div>
-          <div className="latest-img-container">
+          </Link>
+          <Link href="/NorseProjects" className="latest-img-container">
             {" "}
             <img
               src="https://cdn.shopify.com/s/files/1/2102/5145/products/norse-projects-fraser-tab-series-hoodie-scoria-blue-1_480x_crop_center.jpg?v=1666367230"
@@ -30,8 +30,8 @@ const MainPage: FC = () => {
             <div className="middle-text">
               <div>Norse Projects Fraser Hoodie</div>
             </div>
-          </div>
-          <div className="latest-img-container">
+          </Link>
+          <Link href="/Salomon" className="latest-img-container">
             {" "}
             <img
               src="https://cdn.shopify.com/s/files/1/2102/5145/products/andwanderxSalomonXT-6Shoes-1_1200x_crop_center.progressive.jpg?v=1669296832"
@@ -41,8 +41,8 @@ const MainPage: FC = () => {
             <div className="middle-text">
               <div>Salomon XT-6</div>
             </div>
-          </div>
-          <div className="latest-img-container">
+          </Link>
+          <Link href="/Nike" className="latest-img-container">
             {" "}
             <img
               src="https://cdn.shopify.com/s/files/1/2102/5145/products/nike-waffle-one-shoes-summit-white-white-black-orange-1_480x_crop_center.jpg?v=1641891223"
@@ -52,8 +52,8 @@ const MainPage: FC = () => {
             <div className="middle-text">
               <div>Nike Waffle Summit</div>
             </div>
-          </div>
-          <div className="latest-img-container">
+          </Link>
+          <Link href="/Polar" className="latest-img-container">
             <img
               src="https://cdn.shopify.com/s/files/1/1202/6102/products/polar-spiral-shirt-black-white-1_1300x1500_crop_center.progressive.jpg?v=1676295201"
               alt=""
@@ -62,8 +62,8 @@ const MainPage: FC = () => {
             <div className="middle-text">
               <div>Polar Spiral Shirt</div>
             </div>
-          </div>
-          <div className="latest-img-container">
+          </Link>
+          <Link href="/Polar" className="latest-img-container">
             {" "}
             <img
               src="https://cdn.shopify.com/s/files/1/1202/6102/products/polar-big-boy-jeans-light-blue-3_37c29c94-1093-4f50-bba3-1bded6772014_600x696_crop_center.progressive.jpg?v=1647261599"
@@ -73,8 +73,8 @@ const MainPage: FC = () => {
             <div className="middle-text">
               <div>Polar Big Boy Jeans</div>
             </div>
-          </div>
-          <div className="latest-img-container">
+          </Link>
+          <Link href="/NorseProjects" className="latest-img-container">
             {" "}
             <img
               src="https://cdn.shopify.com/s/files/1/2102/5145/products/norse-projects-anton-brushed-flannel-check-shirt-varsity-green-1_480x_crop_center.jpg?v=1666367106"
@@ -84,8 +84,8 @@ const MainPage: FC = () => {
             <div className="middle-text">
               <div>Norse Projects Flannel Shirt</div>
             </div>
-          </div>
-          <div className="latest-img-container">
+          </Link>
+          <Link href="/NorseProjects" className="latest-img-container">
             {" "}
             <img
               src="https://cdn.shopify.com/s/files/1/2102/5145/products/norse-projects-twill-sports-cap-dark-navy-1_5ad3044e-eefc-4ffc-b11a-9c87eca41906_480x_crop_center.jpg?v=1629718580"
@@ -95,7 +95,7 @@ const MainPage: FC = () => {
             <div className="middle-text">
               <div>Norse Proejcts Twill Sports Cap</div>
             </div>
-          </div>
+          </Link>
         </div>
         <div className="salomon-xt-container">
           <div>
