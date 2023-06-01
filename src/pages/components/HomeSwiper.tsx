691b00f2262c4fcfbb1c782d0e809d12f3de1d8c
Jacob Bidmead <107250881+Jacobbidmead@users.noreply.github.com>
2023-06-01 12:47:44 +0100

    Added function that changes background color of featured at set interval

--- src/pages/components/HomeSwiper.tsx
+++ src/pages/components/HomeSwiper.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import mainImg from "@/imageData/mainImgData";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
@@ -6,9 +6,28 @@ import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 interface ImageSliderProps {
   images: string[];
 }
+const colors = [
+  "#FFC0CB",
+  "#FFDAB9",
+  "#FFFFE0",
+  "#E0FFFF",
+  "#D8BFD8",
+  "#0094d0",
+];
 
 const HomeSwiper: React.FC<ImageSliderProps> = ({ images }) => {
   const [currentImageIndex, setCurrentImageIndex] = useState<number>(0);
+  const [colorIndex, setColorIndex] = useState<number>(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setColorIndex((prevColorIndex) => (prevColorIndex + 1) % colors.length);
+    }, 5000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
 
   const previousImage = () => {
     const newIndex = (currentImageIndex - 1 + mainImg.length) % mainImg.length;
@@ -23,7 +42,12 @@ const HomeSwiper: React.FC<ImageSliderProps> = ({ images }) => {
   return (
     <>
       <div className="image-slider">
-        <div className="featured">Youre shop for all things streetwear.</div>
+        <div
+          className="featured"
+          style={{ backgroundColor: colors[colorIndex] }}
+        >
+          Youre shop for all things streetwear.
+        </div>
         <img
           src={mainImg[currentImageIndex]}
           alt="slider"
